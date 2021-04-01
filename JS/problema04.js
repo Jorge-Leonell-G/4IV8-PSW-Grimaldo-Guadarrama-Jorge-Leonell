7c3b098b83148fb7f05f712bcd2cd097c108c15c
Jorge-Leonell-G <jgrimaldog1900@alumno.ipn.mx>
2021-04-01 16:17:11 -0600

    funcionalidades para el problema 4

---
+++ JS/problema04.js
@@ -0,0 +1,20 @@
+/*
+Un alumno desea saber cuál será su calificación final en la materia 
+de Algoritmos. Dicha calificación se compone de los siguientes porcentajes:
+<ul>
+    <li>55% del promedio de sus tres calificaciones parciales</li>
+    <li>30% de la calficación del exámen final.</li>
+    <li>15% de la calificación de un trabajo final</li>
+*/
+
+function parciales{
+
+}
+
+function examen{
+
+}
+
+function trabajo{
+    
+}
