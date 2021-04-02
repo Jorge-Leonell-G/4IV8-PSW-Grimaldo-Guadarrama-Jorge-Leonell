1ffcbd267ecc47503c3e750b0b4c7654a038e46d
Jorge-Leonell-G <jgrimaldog1900@alumno.ipn.mx>
2021-04-01 21:57:18 -0600

    funcionalidad pulida

--- JS/problema01.js
+++ JS/problema01.js
@@ -2,6 +2,10 @@
 vamos a crear una función que se encargue que
 que a partir de una expresión regular se valide la entrada
 de únicamente números
+
+Un individuo desea invertir su capital en un banco y 
+desea saber cuánto dinero ganará después de un mes si el 
+banco paga a razón de 2% mensual.
 */
 
 function validarn(e){
@@ -14,18 +18,38 @@ function validarn(e){
     return patron.test(prueba)
 }
 
+function validarmeses(e){
+    var number = (document.all)?e.keyCode:e.which
+    if(number == 8)return true;
+
+    var patron = /[0-9\d]/;
+
+    var prueba = String.fromCharCode(number);
+    return patron.test(prueba)
+}
+
 function interes(){
     var valor = document.formulario.cantidad.value;
+    var month = document.formulario.mes.value;
     //nuestro valor lo obtenemos como una cadena
     //procedemos a nuestro parseo de cadena a int
     var result = parseInt(valor);
-    var interes = result * 0.02;
-    var total = result+interes;
+    var mensual = parseInt(month)
+    //En nuestra condición if, colocamos "48" que son los numeros de meses 
+    //donde ya hay presencia de intereses fijos
+    if(mensual > 48){
+        alert("Error, ingrese una cantidad de meses válida de entre 1 a 48");
+        document.formulario.sueldoti.value = "";
+    }else{
+        var interes = result * (mensual * 0.02);
+        var total = result+interes;
 
-    document.formulario.sueldoti.value = "$"+total;
+        document.formulario.sueldoti.value = "$"+total;
+    }
 }
 
 function borrar(){
     document.formulario.sueldoti.value = "";
     document.formulario.cantidad.value = "";
+    document.formulario.mes.value = "";
 }
