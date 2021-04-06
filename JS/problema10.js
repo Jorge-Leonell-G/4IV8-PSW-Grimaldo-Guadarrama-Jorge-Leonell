4d56dcc16e14bcf08ea16f707dfadb14ca621c3e
Jorge-Leonell-G <jgrimaldog1900@alumno.ipn.mx>
2021-04-06 16:01:52 -0500

    funciones ultimo problema

---
+++ JS/problema10.js
@@ -0,0 +1,35 @@
+/*
+Calcular la utilidad que un trabajador recibe 
+                    en el reparto anual de utilidades si este se le asigna 
+                    como un porcentaje de su salario mensual que depende de su 
+                    antiguedad en la empresa de acuerdo con la siguiente tabla:
+                    <br>
+                    <table>
+                        <tr>
+                            <td>Tiempo</td>
+                            <td>Utilidad</td>
+                        </tr>
+                        <tr>
+                            <td>Menos de 1 año</td>
+                            <td>5% del salario</td>
+                        </tr>
+                        <tr>
+                            <td>1 año o más y menos de 2 años</td>
+                            <td>7% del salario</td>
+                        </tr>
+                        <tr>
+                            <td>2 años o más y menos de 5 años</td>
+                            <td>10% del salario</td>
+                        </tr>
+                        <tr>
+                            <td>5 años o más y menos de 10 años</td>
+                            <td>15% del salario</td>
+                        </tr>
+                        <tr>
+                            <td>10 años o más</td>
+                            <td>20% del salario</td>
+                        </tr>
+                    </table>
+                </li>
+            </ol>
+*/
